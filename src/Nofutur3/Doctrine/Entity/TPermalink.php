035e143d80f2a4e4c0cf548c4306b5e22ceddc77
Jakub Vyvazil <jakub@n3d.cz>
2017-07-22 09:36:42 +0200

    Added trait for permalink

---
+++ src/Nofutur3/Doctrine/Entity/TPermalink.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * TPermalink.php
+ *
+ * @package  : nofutur3/nette-doctrine
+ * @author   : Jakub Vyvazil <jakub@vyvazil.cz>
+ * @copyright: 2017
+ */
+
+namespace Nofutur3\Doctrine\Entity;
+
+
+class TPermalink
+{
+	/**
+	 * @var string
+	 * @ORM\Column(name="permalink", type="string", length=120, unique=true)
+	 */
+	private $permalink;
+
+	/**
+	 * @return string
+	 */
+	public function getPermalink()
+	{
+		return $this->permalink;
+	}
+
+	/**
+	 * @param string $permalink
+	 */
+	public function setPermalink($permalink)
+	{
+		$this->permalink = Strings::webalize($permalink);
+	}
+
+	/** @ORM\PrePersist */
+	public function savePermalinkOnPrePersist()
+	{
+		if (!$this->permalink && property_exists($this, 'title')) {
+			$this->setPermalink($this->title);
+		}
+	}
+
+	/** @ORM\PreUpdate */
+	public function updatePermalinkOnPreUpdate()
+	{
+		if (!$this->permalink && property_exists($this, 'title')) {
+			$this->setPermalink($this->title);
+		}
+	}
+}
