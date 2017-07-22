779f6a3c11111f2ba72a6b50731283167077c54b
Jakub Vyvazil <jakub@n3d.cz>
2017-07-22 06:46:11 +0200

    Added base entity

---
+++ src/Nofutur3/Doctrine/Entity/BaseEntity.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * BaseEntity.php
+ *
+ * @package  : nofutur3/nette-doctrine
+ * @author   : Jakub Vyvazil <jakub@vyvazil.cz>
+ * @copyright: 2017
+ */
+
+namespace Nofutur3\Doctrine\Entity;
+
+/**
+ * @ORM\MappedSuperclass()
+ * @ORM\HasLifecycleCallbacks()
+ */
+class BaseEntity
+{
+	/**
+	 * @var bool
+	 * @ORM\Column(name="is_enabled", type="boolean")
+	 */
+	protected $enabled;
+
+	/**
+	 * @var \DateTime
+	 * @ORM\Column(name="created_at", type="datetime")
+	 */
+	protected $createdAt;
+
+	/**
+	 * @var \DateTime
+	 * @ORM\Column(name="updated_at", type="datetime")
+	 */
+	protected $updatedAt;
+
+	/**
+	 * BaseEntity constructor.
+	 */
+	public function __construct()
+	{
+		$this->createdAt = new \DateTime();
+		$this->enabled = true;
+	}
+
+	public function __get($property)
+	{
+		if (property_exists($this, $property)) {
+			return $this->$property;
+		}
+	}
+
+	public function __set($property, $value)
+	{
+		if (property_exists($this, $property)) {
+			$this->$property = $value;
+		}
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isEnabled()
+	{
+		return $this->enabled;
+	}
+
+	/**
+	 * @param bool $enabled
+	 */
+	public function setEnabled($enabled)
+	{
+		$this->enabled = $enabled;
+	}
+
+	/**
+	 * @return \DateTime
+	 */
+	public function getCreatedAt()
+	{
+		return $this->createdAt;
+	}
+
+	/**
+	 * @return \DateTime
+	 */
+	public function getUpdatedAt()
+	{
+		return $this->updatedAt;
+	}
+
+	/** @ORM\PrePersist() */
+	public function saveDatetimeOnPrePersist()
+	{
+		if (!$this->createdAt) {
+			$this->createdAt = new \DateTime();
+		}
+		$this->updatedAt = new \DateTime();
+	}
+
+	/** @ORM\PreUpdate() */
+	public function saveDatetimeOnPreUpdate()
+	{
+		$this->updatedAt = new \DateTime();
+	}
+}
