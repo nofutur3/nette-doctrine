117fd501190b3a949e02eadbbe15096175857cb9
Jakub Vyvážil <jakub@vyvazil.cz>
2018-04-14 10:14:04 +0200

    added BaseRepository

---
+++ src/Nofutur3/Doctrine/Repository/BaseRepository.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * BaseRepository.php
+ *
+ * @package  : nofutur3/nette-doctrine
+ * @author   : Jakub Vyvazil <jakub@vyvazil.cz>
+ * @copyright: 2018
+ */
+
+namespace Nofutur3\Doctrine\Repository;
+
+
+use Kdyby\Doctrine\EntityRepository;
+
+class BaseRepository extends EntityRepository
+{
+
+}
